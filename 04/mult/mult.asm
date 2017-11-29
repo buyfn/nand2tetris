7f584afb738f1333ee6dfaa3c0cba94205f9b4c2
ignat <buyfng@gmail.com>
2017-11-29 19:55:31 +0300

    completed project 4

--- 04/mult/mult.asm
+++ 04/mult/mult.asm
@@ -6,4 +6,32 @@
 // Multiplies R0 and R1 and stores the result in R2.
 // (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[2], respectively.)
 
-// Put your code here.
+	@i
+	M=1
+	@product
+	M=0
+(LOOP)
+	@i
+	D=M
+	@1
+	D=D-M
+	@OUT
+	D;JGT
+	@0
+	D=M
+	@product
+	M=D+M
+	@i
+	M=M+1
+	@LOOP
+	0;JMP
+(OUT)
+	@product
+	D=M
+	@2
+	M=D
+	@END
+	0;JMP
+(END)
+	@END
+	0;JMP
